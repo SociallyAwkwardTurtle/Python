86b185cbe6b585d75f272d3d4cd8b05ecf25d849
SociallyAwkwardTurtle <34130549+SociallyAwkwardTurtle@users.noreply.github.com>
2017-12-03 21:31:36 +0200

    Add files via upload

---
+++ Vestluskaaslane.py
@@ -0,0 +1,21 @@
+print("Hello world!")
+print("I am Thonny!")
+print("What is your name?")
+myName = input()
+print("Hello, " + myName + ", it's nice to meet you!")
+print("How old are you?")
+input()
+print("Oh, that's nice!")
+from random import *
+a = (randint(2, 5))
+if a == 2:
+    a=("two")
+elif a == 3:
+    a=("three")
+elif a == 4:
+    a=("four")
+elif a == 5:
+    a=("five")
+print ("I am only " + a + " years old.")
+
+
