b2696f82d71de3dbcf50c44e970f93a481ef146d
SociallyAwkwardTurtle <34130549+SociallyAwkwardTurtle@users.noreply.github.com>
2018-03-13 10:27:27 +0200

    Add files via upload

---
+++ Ül2.py
@@ -0,0 +1,23 @@
+#Karolina 10IT
+#joonestan Jaapani lipu
+
+from turtle import*
+
+forward(400)
+right(90)
+forward(200)
+right(90)
+forward(400)
+right(90)
+forward(200)
+up()
+backward(160)
+right(90)
+forward(200)
+down()
+color("red")
+begin_fill()
+circle(60)
+end_fill()
+
+exitonclick()
