1fe9a721843c96971b5344212a03a36b1d812508
SociallyAwkwardTurtle <34130549+SociallyAwkwardTurtle@users.noreply.github.com>
2018-03-13 10:47:03 +0200

    Add files via upload

---
+++ põdra maja.py
@@ -0,0 +1,17 @@
+#Karolina 10IT
+#joonestan Jaapani lipu
+
+from turtle import*
+shape("turtle")
+forward(300)
+right(90)
+forward(150)
+right(90)
+forward(300)
+right(90)
+forward(150)
+right(45)
+forward(210)
+right(90)
+forward(210)
+exitonclick()
