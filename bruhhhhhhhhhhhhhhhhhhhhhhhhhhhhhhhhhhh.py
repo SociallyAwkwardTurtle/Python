345944c0cadcc07834b7078c152ba014735b76fa
SociallyAwkwardTurtle <34130549+SociallyAwkwardTurtle@users.noreply.github.com>
2018-04-02 09:36:07 +0300

    Add files via upload

---
+++ bruhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh.py
@@ -0,0 +1,25 @@
+# Karolina Noorma
+
+'''from turtle import*
+i = 4
+while i > 0:
+    forward(100)
+    right(90)
+    i -= 1'''
+
+'''from time import*
+a = int(input("Sisestage aeg: "))
+while a > 0:
+    print(a)
+    a -= 1
+    sleep(1)'''
+
+'''kirjutage programm, mis korrutab kasutaja poolt sisestatud 2
+positiivset täisarvu ilma korrutamistehet kasutamata.
+Lisa juurde ka see, et kui kasutaja sisestab negatiivse täisarvu,
+siis programm küsib talt nii kaua uuesti täisarvu sisestamist,
+kuni sisestatakse korrektne arv. 
+Vihje: 4*5= 4+4+4+4+4 või 4*5= 5+5+5+5 '''
+
+a = int(input("Palun sisesta positiivne täisarv: ")
+b = int(input("Palun sisesta teine positiivne täisarv: ")
